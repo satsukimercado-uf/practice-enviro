4a86b0a1821696932a30af20c9933c42b817b405
satsukimercado-uf <satsukimercado@ufl.edu>
2025-08-28 19:53:42 -0400

    Update cat_data.py

--- cat_data.py
+++ cat_data.py
@@ -1,15 +1,21 @@
-# pandas 
-import pandas as pd
+def main():
+  # pandas 
+  import pandas as pd
 
-# define df
-df = pd.DataFrame([
-  ['green', 'M', 10.1, 'class2']
-  ,['red', 'L', 13.5, 'class1']
-  ,['blue', 'XL', 15.3, 'class2']
-])
+  # define df
+  df = pd.DataFrame([
+    ['green', 'M', 10.1, 'class2']
+    ,['red', 'L', 13.5, 'class1']
+    ,['blue', 'XL', 15.3, 'class2']
+  ])
 
-# label columns of df
-df.columns = ['color', 'size', 'price', 'classlbl']
+  # label columns of df
+  df.columns = ['color', 'size', 'price', 'classlbl']
 
-# print df
-df
+  # print df
+  print(df)
+
+
+if __name__ == "__main__":
+  # run main
+    main()
