7214e0409f5c544827773d51e087d511b4f8e35d
satsukimercado-uf <satsukimercado@ufl.edu>
2025-08-28 19:20:08 -0400

    Create cat_data.py

---
+++ cat_data.py
@@ -0,0 +1,15 @@
+# pandas 
+import pandas as pd
+
+# define df
+df = pd.DataFrame([
+  ['green', 'M', 10.1, 'class2']
+  ,['red', 'L', 13.5, 'class1']
+  ,['blue', 'XL', 15.3, 'class2']
+])
+
+# label columns of df
+df.columns = ['color', 'size'. 'price', 'classlbl']
+
+# print df
+df
