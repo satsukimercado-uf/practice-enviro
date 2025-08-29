881389786b0613d107e1e7b1f022d1f2bb7f9500
satsukimercado-uf <satsukimercado@ufl.edu>
2025-08-28 20:23:24 -0400

    Update cat_data.py

--- cat_data.py
+++ cat_data.py
@@ -13,8 +13,28 @@ def main():
   df.columns = ['color', 'size', 'price', 'classlbl']
 
   # print df
+  print("Categorical data encoding with pandas \n")
   print(df)
 
+  # mapping ordinal features dictionary
+  size_mapping = {'XL':3
+                ,'L':2
+                ,'M':1
+                }
+
+  # ivnerse mapping dictionary
+  inv_size_mapping = {v: k for k, v in size_mapping.items()}
+  
+  # apply mapping to column 'size'
+  df['size'] = df['size'].map(size_mapping)
+
+  # print df
+  print("Mapping ordinal features \n")
+  print(df)
+
+# print inv
+print("Inverse size mapping \n")
+print(df['size'].map(inv_size_mapping))
 
 if __name__ == "__main__":
   # run main
